9a1e764ce6ad79d7625bdcfa95a62b164a072daa
davidgo <david.gonzalez.perez@alumnos.uva.es>
2019-01-21 22:30:19 +0100

    First step to setup controller and routes for album

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var app = express();
 // cargar rutas api
 var user_routes = require('./routes/user');
 var artist_routes = require('./routes/artist');
+var album_routes = require('./routes/album');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json()); // para transformar en objetos json las peticiones http que nos llegue
@@ -17,6 +18,7 @@ app.use(bodyParser.json()); // para transformar en objetos json las peticiones h
 // rutas base
 app.use('/api', user_routes);
 app.use('/api', artist_routes);
+app.use('/api', album_routes);
 
 
 module.exports = app;
