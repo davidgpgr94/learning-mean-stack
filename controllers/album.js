351ae4b85f3e6e1597733d3ea7a73d9f7bcd82ca
davidgo <david.gonzalez.perez@alumnos.uva.es>
2019-01-21 23:26:31 +0100

    Created method to upload and get an album's song

--- controllers/album.js
+++ controllers/album.js
@@ -117,10 +117,58 @@ function deleteAlbum(req, res) {
 
 };
 
+function uploadImage(req, res) {
+    var albumId = req.params.id;
+    var file_name = 'No subido...';
+
+    if(req.files) {
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        if (file_ext == 'png' || file_ext == 'gif' || file_ext == 'jpg') {
+            Album.findByIdAndUpdate(albumId, {image: file_name}, (err, albumUpdated) => {
+                if (err) {
+                    res.status(500).send({message: 'Error al actualizar el album'});
+                } else {
+                    if (!albumUpdated) {
+                        res.status(404).send({message: 'No se ha podido actualizar el album'});
+                    } else {
+                        res.status(200).send({album: albumUpdated});
+                    }
+                }
+            });
+        } else {
+            res.status(200).send({message: 'Extensión del archivo no válida'});
+        }
+
+        console.log(ext_split);
+    } else {
+        res.status(200).send({message: 'No has subido ninguna imágen...'});
+    }
+}
+
+function getImageFile(req, res) {
+    var imageFile = req.params.imageFile;
+    var path_file = './uploads/albums/'+imageFile;
+    fs.exists(path_file, (exists) => {
+        if (exists) {
+            res.sendFile(path.resolve(path_file));
+        } else {
+            res.status(200).send({message: 'No existe la imagen...'});
+        }
+    });
+};
+
 module.exports = {
     getAlbum,
     getAlbums,
     saveAlbum,
     updateAlbum,
-    deleteAlbum
+    deleteAlbum,
+    uploadImage,
+    getImageFile
 }
