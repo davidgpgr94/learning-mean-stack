e05fa22c9682c64d0db7db5d9baeef0a1d17f2f6
davidgo <david.gonzalez.perez@alumnos.uva.es>
2019-01-21 17:43:59 +0100

    Created method to update an avatar image

--- controllers/user.js
+++ controllers/user.js
@@ -102,9 +102,44 @@ function updateUser(req, res) {
     });
 }
 
+function uploadImage(req, res) {
+    var userId = req.params.id;
+    var file_name = 'No subido...';
+
+    if(req.files) {
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        if (file_ext == 'png' || file_ext == 'gif' || file_ext == 'jpg') {
+            User.findByIdAndUpdate(userId, {image: file_name}, (err, userUpdated) => {
+                if (err) {
+                    res.status(500).send({message: 'Error al actualizar el usuario'});
+                } else {
+                    if (!userUpdated) {
+                        res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+                    } else {
+                        res.status(200).send({user: userUpdated});
+                    }
+                }
+            });
+        } else {
+            res.status(200).send({message: 'Extensión del archivo no válida'});
+        }
+
+        console.log(ext_split);
+    } else {
+        res.status(200).send({message: 'No has subido ninguna imágen...'});
+    }
+}
+
 module.exports = {
     pruebas,
     saveUser,
     loginUser,
-    updateUser
+    updateUser,
+    uploadImage
 };
