e829a136592c12a2fea71feac7ac1da992004997
davidgo <david.gonzalez.perez@alumnos.uva.es>
2019-01-21 18:20:21 +0100

    Created method to create artist and get an artist by id

--- controllers/artist.js
+++ controllers/artist.js
@@ -8,9 +8,43 @@ var Album = require('../models/album');
 var Song = require('../models/song');
 
 function getArtist(req, res) {
-    res.status(200).send({message: 'Metodo getArtist del controlador de Artist'});
+    var artistId = req.params.id;
+
+    Artist.findById(artistId, (err, artist) => {
+        if (err) {
+            res.status(500).send({message: 'Error en la petición'});
+        } else {
+            if (!artist) {
+                res.status(404).send({message: 'El artista no existe'});
+            } else {
+                res.status(200).send({artist});
+            }
+        }
+    });
+};
+
+function saveArtist(req, res) {
+    var artist = new Artist();
+
+    var params = req.body;
+    artist.name = params.name;
+    artist.description = params.description;
+    artist.image = 'null';
+
+    artist.save((err, artistStored) => {
+        if (err) {
+            res.status(500).send({message: 'Error al guardar el artista'});
+        } else {
+            if (!artistStored) {
+                res.status(404).send({message: 'El artista no ha sido guardado'});
+            } else {
+                res.status(200).send({artist: artistStored});
+            }
+        }
+    });
 };
 
 module.exports = {
-    getArtist
+    getArtist,
+    saveArtist
 }
