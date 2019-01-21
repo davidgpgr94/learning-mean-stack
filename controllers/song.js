0e76680e0de87b39f3272a940dc29aa0f5981278
davidgo <david.gonzalez.perez@alumnos.uva.es>
2019-01-22 00:26:48 +0100

    Created method to update song info

--- controllers/song.js
+++ controllers/song.js
@@ -78,11 +78,29 @@ function saveSong(req, res) {
                 res.status(200).send({song: songStored});
             }
         }
-    })
+    });
 };
+
+function updateSong(req, res) {
+    var songId = req.params.id;
+    var update = req.body;
+
+    Song.findByIdAndUpdate(songId, update, (err, songUpdated) => {
+        if (err) {
+            res.status(500).send({message: 'Error en el servidor'});
+        } else {
+            if (!songUpdated) {
+                res.status(404).send({message: 'No se ha actualizado la canción'});
+            } else {
+                res.status(200).send({song: songUpdated});
+            }
+        }
+    });
+}
 
 module.exports = {
     getSong,
     getSongs,
-    saveSong
+    saveSong,
+    updateSong
 };
