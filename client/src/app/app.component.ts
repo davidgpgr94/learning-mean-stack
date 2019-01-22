4bc891aed0f2fee263a5e97dee4de1d18adfe2be
davidgo <david.gonzalez.perez@alumnos.uva.es>
2019-01-22 13:52:04 +0100

    First setup to the main component that will have the login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app works!';
+  public title = 'MUSIFY';
+  public user: User;
+  public identity = true;
+  public token;
+
+  constructor() {
+
+    this.user = new User('', '', '', '', '', 'ROLE_USER', '');
+  }
+
 }
