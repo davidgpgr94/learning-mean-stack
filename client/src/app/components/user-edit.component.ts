9e6878c62df4d745dfd961be3714476fcace2013
davidgo <david.gonzalez.perez@alumnos.uva.es>
2019-01-24 14:30:57 +0100

    Upload image user

--- client/src/app/components/user-edit.component.ts
+++ client/src/app/components/user-edit.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {UserService} from '../services/user.service';
 import { User } from '../models/user';
 
+import {GLOBAL} from '../services/global';
+
 @Component({
   selector: 'user-edit',
   templateUrl: '../views/user-edit.component.html',
@@ -13,6 +15,8 @@ export class UserEditComponent implements OnInit {
     public identity;
     public token;
     public alertMessage;
+    public filesToUpload: Array<File>;
+    public url: string;
 
     constructor(private _userService: UserService) {
         this.titulo = 'Actualizar mis datos';
@@ -20,6 +24,7 @@ export class UserEditComponent implements OnInit {
         this.identity = this._userService.getIdentity();
         this.token = this._userService.getToken();
         this.user = this.identity;
+        this.url = GLOBAL.url;
     }
 
     ngOnInit(): void {
@@ -34,6 +39,17 @@ export class UserEditComponent implements OnInit {
                 } else {
                     localStorage.setItem('identity', JSON.stringify(this.user));
                     document.getElementById('identity_name').innerHTML = this.user.name;
+                    if (!this.filesToUpload) {
+                        // redireccion
+                    } else {
+                        this.makeFileRequest(this.url + 'upload-image-user/' + this.user._id, [], this.filesToUpload).then(
+                            (result: any) => {
+                                this.user.image = result.image;
+                                localStorage.setItem('identity', JSON.stringify(this.user));
+                                console.log(this.user);
+                            }
+                        );
+                    }
                     this.alertMessage = 'Datos actualizados correctamente';
                 }
             },
@@ -46,5 +62,35 @@ export class UserEditComponent implements OnInit {
                 }
             }
         );
+    }
+
+    fileChangeEvent(fileInput: any) {
+        this.filesToUpload = <Array<File>>fileInput.target.files;
+    }
+
+    makeFileRequest(url: string, params: Array<string>, files: Array<File>) {
+        const token = this.token;
+        return new Promise(function(resolve, reject) {
+            var formData: any = new FormData();
+            var xhr = new XMLHttpRequest();
+
+            for (var i = 0; i < files.length; i++) {
+                formData.append('image', files[i], files[i].name);
+            }
+
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        resolve(JSON.parse(xhr.response));
+                    } else {
+                        reject(xhr.response);
+                    }
+                }
+            };
+
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('Authorization', token);
+            xhr.send(formData);
+        });
     }
 }
